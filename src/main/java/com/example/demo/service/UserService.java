3769fcc6ffa116abcb7a2597f2a77d8c32b3bc45
NSQA <nsqa99@github.com>
2021-05-24 16:24:52 +0700

    get all orders by user id

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,15 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.OrderDto;
 import com.example.demo.dto.UserDto;
 
 @Service
 public interface UserService {
 	public UserDto getCurrentUser(Long id);
+	public List<OrderDto> getOrdersByUserId(long id, Pageable pageable);
 }
